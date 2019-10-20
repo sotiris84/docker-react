cf9779cdd39a30d31dce6f05313da58aaf28b26e
sotiris84 <skaragiorgis@gmail.com>
2019-10-20 22:17:29 +0300

    Initial Commit

---
+++ build/precache-manifest.3bb4f24a04a4c00826433b734f410acf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f086ed759b3b802d173898fd772c76d6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "667b695b43be2d7cfac9",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "b9fc779205c862c0b8aa",
+    "url": "/static/js/2.760844ff.chunk.js"
+  },
+  {
+    "revision": "667b695b43be2d7cfac9",
+    "url": "/static/js/main.1de41dfc.chunk.js"
+  },
+  {
+    "revision": "ed4750f41f4543566353",
+    "url": "/static/js/runtime-main.fbe98da3.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
